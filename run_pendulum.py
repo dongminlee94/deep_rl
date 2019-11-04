3008f4bea0850491a23bdd466c1082bec8d17b31
dongminleeai <kid33629@gmail.com>
2019-11-04 23:31:19 +0900

    Update..

--- run_pendulum.py
+++ run_pendulum.py
@@ -139,15 +139,18 @@ def main():
             if eval_average_return >= args.threshold_return:
                 if not os.path.exists('./tests/save_model'):
                     os.mkdir('./tests/save_model')
-                    os.mkdir('./tests/save_model/' + 'Pendulum-v0')
-                    os.mkdir('./tests/save_model/' + 'Pendulum-v0/' + args.algo)
+                    # os.mkdir('./tests/save_model/' + 'Pendulum-v0')
+                    # os.mkdir('./tests/save_model/' + 'Pendulum-v0/' + args.algo)
                 
-                ckpt_path = os.path.join('./tests/save_model/' + 'Pendulum-v0/' + args.algo + '/' \
+                ckpt_path = os.path.join('./tests/save_model/' + 'Pendulum-v0_' + args.algo + '_' \
                                                                                 + '_ep_' + str(train_num_episodes) \
                                                                                 + '_rt_' + str(round(eval_average_return, 2)) \
                                                                                 + '_t_' + str(int(time.time() - start_time)) + '.pt')
                 
-                torch.save(agent.actor.state_dict(), ckpt_path)
+                torch.save({
+                    'actor': actor.state_dict(),
+                    'critic': critic.state_dict()
+                }, ckpt_path)
 
 if __name__ == "__main__":
     main()
