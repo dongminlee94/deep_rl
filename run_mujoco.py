c5697d1147b0b6ecbad0ce123531f3098074fb35
dongminleeai <kid33629@gmail.com>
2019-11-10 12:20:07 +0900

    Update..

--- run_mujoco.py
+++ run_mujoco.py
@@ -11,7 +11,7 @@ from torch.utils.tensorboard import SummaryWriter
 parser = argparse.ArgumentParser(description='RL algorithms with PyTorch in MuJoCo environments')
 parser.add_argument('--env', type=str, default='HalfCheetah-v2', 
                     help='choose an environment between HalfCheetah-v2, Ant-v2 and Humanoid-v2')
-parser.add_argument('--algo', type=str, default='tac', 
+parser.add_argument('--algo', type=str, default='atac', 
                     help='select an algorithm among vpg, npg, trpo, ppo, ddpg, td3, sac, asac, tac, atac')
 parser.add_argument('--seed', type=int, default=0, 
                     help='seed for random number generators')
