df7810c85bcf6375c35b295199a2c83d395ae2f7
dongminleeai <kid33629@gmail.com>
2020-06-25 14:52:03 +0900

    Modify networks.py

--- tests/common/networks.py
+++ tests/common/networks.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.distributions import Categorical, Normal
-from agents.common.utils import identity
+from common.utils import identity
 
 
 """
